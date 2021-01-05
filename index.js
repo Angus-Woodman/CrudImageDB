f0d921679848bd1314b87de86e826f96b5512d00
Angus Woodman <>
2021-01-05 12:14:46 +0000

    initial installs and express server setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Connect DB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("mongoDB is connected"))
+  .catch((err) => console.log(err)); 
+
+// Middleware
+app.use(express.json());
+
+app.listen(5000, () => console.log("Server is running"));
